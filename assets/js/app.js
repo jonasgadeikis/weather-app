6c200def48f055225e5a8ee6df6a5e2f95a3e73e
jonasgadeikis <jonasgadeikis.github@gmail.com>
2020-01-12 14:37:53 +0200

    Boilerplate commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+import '../css/app.scss';
+import Vue from 'vue';
+import App from './App';
+import router from './router/index';
+import store from './store/index';
+
+new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+});
