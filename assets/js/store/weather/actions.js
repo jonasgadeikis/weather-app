9078874a2b41dc22a59c6886b25eb5e9170f4763
jonasgadeikis <jonasgadeikis.github@gmail.com>
2020-01-15 11:12:11 +0200

    Edit front-end validation so the back-end won't be called when dublicate city given

--- assets/js/store/weather/actions.js
+++ assets/js/store/weather/actions.js
@@ -1,29 +1,37 @@
 import axios from "axios";
 
 export default {
-    getData({commit}, payload) {
-        commit('toggleLoadingState');
+    getData({commit, state}, payload) {
         commit('hideErrorMessage');
-        axios.get('/api/weather', {
-            params: {
-                city: payload.city,
-                apiKey: payload.apiKey,
-            }
-        }).then(response => {
-            const data = {
-                name: response.data.name,
-                temp: response.data.temp,
-            };
-
-            commit('setWeatherData', data);
-        }).catch(error => {
-            const data = {
-                message: error.response.data.message,
-            };
-
-            commit('showErrorMessage', data);
-        }).finally(() => {
-            commit('toggleLoadingState');
+        const duplicate = state.weatherData.find(city => {
+            return city.name === payload.city;
         });
+
+        if (typeof duplicate === 'undefined') {
+            commit('toggleLoadingState');
+            axios.get('/api/weather', {
+                params: {
+                    city: payload.city,
+                    apiKey: payload.apiKey,
+                }
+            }).then(response => {
+                const data = {
+                    name: response.data.name,
+                    temp: response.data.temp,
+                };
+
+                commit('setWeatherData', data);
+            }).catch(error => {
+                const data = {
+                    message: error.response.data.message,
+                };
+
+                commit('showErrorMessage', data);
+            }).finally(() => {
+                commit('toggleLoadingState');
+            });
+        } else {
+            state.errorMessage = 'Duplicate city given.';
+        }
     },
 }
